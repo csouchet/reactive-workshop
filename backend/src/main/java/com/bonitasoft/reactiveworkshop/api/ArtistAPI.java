ae4e1121caeaa78d439d9159feb8a4a93fc162c2
Souchet Céline <souchet.celine.5@gmail.com>
2019-01-08 15:35:42 +0100

    - Create the Rest service to find an artist with its 10 last comments - Create unit tests

--- backend/src/main/java/com/bonitasoft/reactiveworkshop/api/ArtistAPI.java
+++ backend/src/main/java/com/bonitasoft/reactiveworkshop/api/ArtistAPI.java
@@ -5,32 +5,76 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.bonitasoft.reactiveworkshop.domain.artist.Artist;
+import com.bonitasoft.reactiveworkshop.domain.artist.ArtistViews;
+import com.bonitasoft.reactiveworkshop.domain.artist.ArtistViews.WithComments;
+import com.bonitasoft.reactiveworkshop.domain.artist.ArtistViews.WithoutComments;
+import com.bonitasoft.reactiveworkshop.domain.comment.Comment;
 import com.bonitasoft.reactiveworkshop.exception.NotFoundException;
 import com.bonitasoft.reactiveworkshop.repository.ArtistRepository;
+import com.fasterxml.jackson.annotation.JsonView;
+
+import reactor.core.publisher.Mono;
 
 @RestController
 public class ArtistAPI {
 
+	private static final String ENDPOINT_10_LAST_COMMENTS_BY_ARTIST = "/comments/{artistId}/last10";
 
-    private ArtistRepository artistRepository;
-    private RestTemplate restTemplate;
+	private final ArtistRepository artistRepository;
 
-    public ArtistAPI(ArtistRepository artistRepository, RestTemplate restTemplate) {
-        this.artistRepository = artistRepository;
-        this.restTemplate = restTemplate;
-    }
+	private final WebClient webClient;
 
-    @GetMapping("/artist/{id}")
-    public Artist findById(@PathVariable String id) throws NotFoundException {
-        return artistRepository.findById(id).orElseThrow(NotFoundException::new);
-    }
+	public ArtistAPI(final ArtistRepository artistRepository, final WebClient webClient) {
+		this.artistRepository = artistRepository;
+		this.webClient = webClient;
+	}
 
-    @GetMapping("/artists")
-    public List<Artist> findAll() throws NotFoundException {
-        return artistRepository.findAll();
-    }
+	@JsonView(ArtistViews.WithoutComments.class)
+	@GetMapping("/artists")
+	public List<Artist> findAll() {
+		return artistRepository.findAll();
+	}
+
+	@JsonView(ArtistViews.WithoutComments.class)
+	@GetMapping("/artist/{id}")
+	public Artist findById(@PathVariable final String id) throws NotFoundException {
+		return artistRepository.findById(id)
+				.orElseThrow(NotFoundException::new);
+	}
+
+	/**
+	 * Find an artist with its 10 last comments
+	 *
+	 * @param id
+	 *            The identifier of the artist to find
+	 * @return The artist with its 10 last comments
+	 * @throws NotFoundException
+	 *             Throws when the artist is not found
+	 */
+	@JsonView(ArtistViews.WithComments.class)
+	@GetMapping("/artist/{id}/comments")
+	public Mono<Artist> findByIdWith10LastComments(@PathVariable final String id) throws NotFoundException {
+		final Mono<Artist> artistFlux = Mono.just(findById(id));
+		final Mono<List<Comment>> commentsFlux = webClient.get()
+				.uri(ENDPOINT_10_LAST_COMMENTS_BY_ARTIST, id)
+				.retrieve()
+				.bodyToFlux(Comment.class)
+				.log()
+				.collectList();
+
+		// The zip method allows to easily combine the results of several Mono
+		// with the great benefit that the execution of your zip method will
+		// last as much as the longest Mono , not the sum of all the executions.
+		return artistFlux.zipWith(commentsFlux)
+				.map(tuple -> {
+					final Artist artist = tuple.getT1();
+					final List<Comment> comments = tuple.getT2();
+					artist.setComments(comments);
+					return artist;
+				});
+	}
 
 }
---
+++ backend/src/test/java/com/bonitasoft/reactiveworkshop/api/ArtistAPITest.java
@@ -0,0 +1,185 @@
+package com.bonitasoft.reactiveworkshop.api;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+import java.util.Optional;
+
+import org.csouchet.test.extension.TestStatusLoggerExtension;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClient.RequestHeadersSpec;
+import org.springframework.web.reactive.function.client.WebClient.RequestHeadersUriSpec;
+import org.springframework.web.reactive.function.client.WebClient.ResponseSpec;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+
+import com.bonitasoft.reactiveworkshop.domain.artist.Artist;
+import com.bonitasoft.reactiveworkshop.domain.comment.Comment;
+import com.bonitasoft.reactiveworkshop.repository.ArtistRepository;
+
+import reactor.core.publisher.Flux;
+
+/**
+ * @author SOUCHET Céline
+ *
+ */
+@ExtendWith({TestStatusLoggerExtension.class, SpringExtension.class})
+@WebFluxTest(ArtistAPI.class)
+public class ArtistAPITest {
+
+	@Autowired
+	private WebTestClient webTestClient;
+
+	@MockBean
+	private ArtistRepository artistRepository;
+
+	@MockBean
+	private WebClient webClient;
+
+	/**
+	 * Test method for
+	 * {@link com.bonitasoft.reactiveworkshop.api.ArtistAPI#findByIdWith10LastComments(java.lang.String)}.
+	 */
+	@SuppressWarnings({"rawtypes", "unchecked"})
+	@Test
+	@DisplayName("findByIdWith10LastComments() should return the artist linked to its 10 last comments when the External Service returns the comments")
+	public void findByIdWith10LastComments_should_return_artist_linked_to_10_last_comments_when_external_service_returns_comments() {
+		// Given
+		final String id = "2";
+		final Artist artist = Artist.builder()
+				.id(id)
+				.name("name")
+				.genre("genre")
+				.build();
+		given(artistRepository.findById(id)).willReturn(Optional.of(artist));
+
+		final Comment comment = new Comment(new Artist(), "user name", "message");
+		final RequestHeadersUriSpec headersUriSpec = mock(RequestHeadersUriSpec.class);
+		final RequestHeadersSpec headersSpec = mock(RequestHeadersSpec.class);
+		final ResponseSpec responseSpec = mock(ResponseSpec.class);
+		given(webClient.get()).willReturn(headersUriSpec);
+		given(headersUriSpec.uri("/comments/{artistId}/last10", id)).willReturn(headersSpec);
+		given(headersSpec.retrieve()).willReturn(responseSpec);
+		given(responseSpec.bodyToFlux(Comment.class)).willReturn(Flux.just(comment));
+
+		// When
+		final Artist result = webTestClient.get()
+				.uri("/artist/{id}/comments", id)
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus()
+				.isOk()
+				.expectBody(Artist.class)
+				.returnResult()
+				.getResponseBody();
+
+		// Then
+		assertThat(result).isEqualTo(artist);
+		assertThat(result.getComments()).containsExactly(comment);
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.bonitasoft.reactiveworkshop.api.ArtistAPI#findByIdWith10LastComments(java.lang.String)}.
+	 */
+	@SuppressWarnings({"rawtypes", "unchecked"})
+	@Test
+	@DisplayName("findByIdWith10LastComments() should return the artist with a empty list of comments when the External Service returns a empty list of comments")
+	public void findByIdWith10LastComments_should_return_artist_with_empty_comments_when_external_service_returns_NO_comments() {
+		// Given
+		final String id = "2";
+		final Artist artist = Artist.builder()
+				.id(id)
+				.name("name")
+				.genre("genre")
+				.build();
+		given(artistRepository.findById(id)).willReturn(Optional.of(artist));
+
+		final RequestHeadersUriSpec headersUriSpec = mock(RequestHeadersUriSpec.class);
+		final RequestHeadersSpec headersSpec = mock(RequestHeadersSpec.class);
+		final ResponseSpec responseSpec = mock(ResponseSpec.class);
+		given(webClient.get()).willReturn(headersUriSpec);
+		given(headersUriSpec.uri("/comments/{artistId}/last10", id)).willReturn(headersSpec);
+		given(headersSpec.retrieve()).willReturn(responseSpec);
+		given(responseSpec.bodyToFlux(Comment.class)).willReturn(Flux.empty());
+
+		// When
+		final Artist result = webTestClient.get()
+				.uri("/artist/{id}/comments", id)
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus()
+				.isOk()
+				.expectBody(Artist.class)
+				.returnResult()
+				.getResponseBody();
+
+		// Then
+		assertThat(result).isEqualTo(artist);
+		assertThat(result.getComments()).isEmpty();
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.bonitasoft.reactiveworkshop.api.ArtistAPI#findByIdWith10LastComments(java.lang.String)}.
+	 */
+	@SuppressWarnings({"rawtypes", "unchecked"})
+	@Test
+	@DisplayName("findByIdWith10LastComments() should return a response with internal error when the External Service returns 4xx or 5xx status code")
+	public void findByIdWith10LastComments_should_return_internal_error_when_bodyToFlux_throws_WebClientResponseException() {
+		// Given
+		final String id = "2";
+		final Artist artist = Artist.builder()
+				.id(id)
+				.name("name")
+				.genre("genre")
+				.build();
+		given(artistRepository.findById(id)).willReturn(Optional.of(artist));
+
+		final RequestHeadersUriSpec headersUriSpec = mock(RequestHeadersUriSpec.class);
+		final RequestHeadersSpec headersSpec = mock(RequestHeadersSpec.class);
+		final ResponseSpec responseSpec = mock(ResponseSpec.class);
+		given(webClient.get()).willReturn(headersUriSpec);
+		given(headersUriSpec.uri("/comments/{artistId}/last10", id)).willReturn(headersSpec);
+		given(headersSpec.retrieve()).willReturn(responseSpec);
+		given(responseSpec.bodyToFlux(Comment.class)).willThrow(WebClientResponseException.class);
+
+		// When // Then
+		webTestClient.get()
+				.uri("/artist/{id}/comments", id)
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus()
+				.is5xxServerError();
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.bonitasoft.reactiveworkshop.api.ArtistAPI#findByIdWith10LastComments(java.lang.String)}.
+	 */
+	@Test
+	@DisplayName("findByIdWith10LastComments() should return a 404 response when there is no artist corresponding to the id")
+	public void findByIdWith10LastComments_should_return_404_status_when_artist_NOT_exist() {
+		// Given
+		final String id = "2";
+		given(artistRepository.findById(id)).willReturn(Optional.empty());
+
+		// When // Then
+		webTestClient.get()
+				.uri("/artist/{id}/comments", id)
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus()
+				.isNotFound();
+	}
+
+}
